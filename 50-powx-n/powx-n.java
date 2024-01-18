82e175f967630542f85668f35088d86f7d433b49
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2024-01-18 22:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (10.30%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -6,7 +6,7 @@ class Solution {
     }
 double po(double x, int n) {
         if(n == 0) return 1;
-        if(n == 1) return x;
+        
         double half = po(x, n/2);
         if(n % 2 == 0) {
             return half * half;
