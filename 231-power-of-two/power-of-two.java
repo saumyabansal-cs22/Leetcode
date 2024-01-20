6929082e0c78d8f28bc7dafcd54af72097aeccf2
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2024-01-20 23:06:29 +0530

    Time: 1 ms (69.50%) | Memory: 40.7 MB (33.02%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+         while(n>1 && n%2==0){
+            n=n/2;
+        }
+        if (n==1){
+            return true;
+        }
+        return false;
+    }
+}
