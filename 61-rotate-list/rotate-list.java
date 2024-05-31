e5f9e2c58bb4245a4a176d60fc0107bd0907c027
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2024-05-31 22:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (39.29%) - LeetSync

---
+++ 61-rotate-list/rotate-list.java
@@ -0,0 +1,51 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || k == 0) {
+            return head;
+        }
+
+        // Calculate the length of the list
+        int len = size(head);
+        if (len == 0) {
+            return head;
+        }
+
+        // Compute the effective number of rotations needed
+        k = k % len;
+        if (k == 0) {
+            return head;
+        }
+
+        // Find the new tail: (len - k - 1)th node
+        // and the new head: (len - k)th node
+        ListNode newTail = getnode(head,len-k-1);
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        // Find the end of the rotated part and connect it to the old head
+        ListNode temp = newHead;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = head;
+
+        return newHead;
+    }
+
+    public int size(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            head = head.next;
+            count++;
+        }
+        return count;
+    }
+    public ListNode getnode(ListNode head,int k){
+        ListNode temp=head;
+        for(int i=0;i<k;i++){
+            temp=temp.next;
+        }
+        return temp;
+    }
+}
