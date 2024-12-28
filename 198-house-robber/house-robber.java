c420f5935ba6d2b3df19c86e6a8571ae4d7d7ab4
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2024-12-28 19:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (62.35%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,22 @@
-
-
-
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
+        int n= nums.length;
+        int[] dp = new int[nums.length];
+        if (n==0){
             return 0;
         }
-
-        int[] s0 = new int[2];
-        int[] s1 = new int[2];
-        s1[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            s0[i % 2] = Math.max(s0[(i - 1) % 2], s1[(i - 1) % 2]);
-            s1[i % 2] = s0[(i - 1) % 2] + nums[i];
+        if (n==1){
+            return nums[0];
         }
-
-        return Math.max(s0[(n - 1) % 2], s1[(n - 1) % 2]);
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        int ch=0,nch=0;
+        for(int i=2;i<n;i++){
+            ch= nums[i]+dp[i-2];
+            nch=dp[i-1];
+            dp[i]=Math.max(ch,nch);
+        }
+        return dp[n-1];
     }
+       
 }
