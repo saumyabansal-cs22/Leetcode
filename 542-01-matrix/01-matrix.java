92dca0fe9e5745214fd4836c8305f5a0d99a9f0d
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2025-06-21 23:15:27 +0530

    Time: 13 ms (85.88%) | Memory: 47 MB (79.75%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,38 +1,51 @@
+class Triple{
+    int first;
+    int second;
+    int third;
+    public Triple(int first, int second, int third){
+        this.first=first;
+        this.second=second;
+        this.third=third;   
+    }
+}
+
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[][] res = new int[n][m];
-        boolean[][] vis = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
+        int n= mat.length;
+        int m= mat[0].length;
+        int[][] vis= new int[n][m];
+        int[][] dist= new int[n][m];
+        Queue<Triple> q= new LinkedList<Triple>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if (mat[i][j]==0){
+                    q.add(new Triple(i,j,0));
+                    vis[i][j]=1;
+                }
+                else{
+                    vis[i][j]=0;
+                }
+            }
+        } 
+        int[] deltar= {-1,0,1,0};
+        int[] deltac= {0,1,0,-1};
 
-        int[] deltar = {-1, 0, 1, 0};
-        int[] deltac = {0, 1, 0, -1};
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    q.offer(new int[]{i, j});
-                    vis[i][j] = true;
+        while(!q.isEmpty()){
+            Triple rr= q.peek();
+            int row= rr.first;
+            int col= rr.second;
+            int steps= rr.third;
+            q.remove();
+            dist[row][col]=steps;
+            for(int i=0;i<4;i++){
+                int nrow= row+deltar[i];
+                int ncol= col+deltac[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.add(new Triple(nrow, ncol,steps+1));
                 }
             }
         }
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int row = curr[0];
-            int col = curr[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + deltar[i];
-                int ncol = col + deltac[i];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol]) {
-                    res[nrow][ncol] = res[row][col] + 1;
-                    vis[nrow][ncol] = true;
-                    q.offer(new int[]{nrow, ncol});
-                }
-            }
-        }
-
-        return res;
+        return dist;  
     }
 }
