f4ef9e1844eb2f024ca5a795008733137900d1b5
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2025-08-22 12:03:45 +0530

    Time: 1 ms (80.97%) | Memory: 41.8 MB (32.52%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,29 +1,25 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
+       String[] arr1= version1.split("\\.");
+       String[] arr2= version2.split("\\.");
 
-        int length = Math.max(v1.length, v2.length);
-
-        for (int i = 0; i < length; i++) {
-            int num1 = 0;
-            int num2 = 0;
-
-            if (i < v1.length) {
-                num1 = Integer.parseInt(v1[i]);
-            }
-
-            if (i < v2.length) {
-                num2 = Integer.parseInt(v2[i]);
-            }
-
-            if (num1 > num2) {
-                return 1;
-            } else if (num1 < num2) {
-                return -1;
-            }
-        }
-
-        return 0;
+       int len= Math.max(arr1.length,arr2.length);
+       for(int i=0;i<len;i++){
+           int n1=0;
+           int n2=0;
+           if(i<arr1.length){
+              n1=Integer.parseInt(arr1[i]);
+           }
+           if(i<arr2.length){
+            n2=Integer.parseInt(arr2[i]);
+           }
+           if(n1<n2){
+            return -1;
+           }
+           else if(n1>n2){
+            return 1;
+           }
+       }
+       return 0;   
     }
 }
